022d8bff1dfe6a3a3abac6d53f23d056f72a7dd0
garima0901-source <garima0901@gmail.com>
2026-04-10 11:33:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
-import ComplianceDashboard from './ComplianceDashboard'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-export default function App() {
-  return <ComplianceDashboard />
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
